c41d55b6da3a1d68bf00c3875add918f2d33f785
SYS <arya00014@gmail.com>
2021-03-16 21:48:01 +0530

    Conneted program to singleton program to explain how it works

---
+++ src/com/advanced/dp/singleton/Apps.java
@@ -0,0 +1,13 @@
+package com.advanced.dp.singleton;
+
+public class Apps {
+public static void main(String[] args) {
+	Normal obj = new Normal();
+	obj.print();
+	/*Singleton object = new Singleton(); */                     /*here singleton object is not created because ,
+	                                                           we are allowed to create only one object
+	                                                            that's why it is singleton,so we keep its
+	                                                             access modifier as private*/
+	                                                         
+}
+}
